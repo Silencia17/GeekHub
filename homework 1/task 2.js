09dde85d5dafdf74040e21aa2e52a6284aa00677
Silencia17 <60505498+Silencia17@users.noreply.github.com>
2020-11-13 09:52:13 +0200

    Update task 2.js

--- homework 1/task 2.js
+++ homework 1/task 2.js
@@ -1,23 +1,23 @@
-var heigh = [2,1,5,0,3,4,7,2,3,1,0];
+var height = [2,1,5,0,3,4,7,2,3,1,0];
 
 	var leftMax = 0;
 	var rightMax = 0;
 	var left = 0;
-	var right = heigh.length - 1;
+	var right = height.length - 1;
 	var volume = 0;
 		
 		while(left < right) {
-			if(heigh[left] > leftMax) {
-				leftMax = heigh[left];
+			if(height[left] > leftMax) {
+				leftMax = height[left];
 			}
-			if(heigh[right] > rightMax) {
-				rightMax = heigh[right];
+			if(height[right] > rightMax) {
+				rightMax = height[right];
 			}
 			if(leftMax >= rightMax) {
-				volume += rightMax - heigh[right];
+				volume += rightMax - height[right];
 				right--;
 			} else {
-				volume += leftMax - heigh[left];
+				volume += leftMax - height[left];
 				left++;
 			}
 		}
