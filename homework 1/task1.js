47091d1bb2843851c932b31ed0219c84ab382d6a
Silencia17 <60505498+Silencia17@users.noreply.github.com>
2020-11-13 08:54:38 +0200

    Update task1.js

--- homework 1/task1.js
+++ homework 1/task1.js
@@ -1,4 +1,4 @@
-var arr = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+var arr = [3,-5,1,44,-12,3,1,2,-3,-3,2,1,4,-2-3-1];
 //var arr = [-1,-8,2];
 //var arr = [1,7,3];
 //var arr = [1,undefined,3,5,-3];
@@ -6,35 +6,39 @@ var arr = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
 
 //max
 
-function maxValue(){
+function maxValue(arr){
     currentValue = arr[0];
-      for(var i = 0; i < arr.length; i++){
+    for(var i = 0; i < arr.length; i++){
         if(arr[i] > currentValue){
-        currentValue = arr[i];
-        }}
-      }
-maxValue()
-console.log('max:' + ' ' + currentValue)
+            currentValue = arr[i];
+        }
+    }
+    return currentValue;
+}
+console.log('max:' + ' ' + maxValue(arr))
 
 //min
 
-function minValue(){
+function minValue(arr){
     currentValue = arr[0];
-      for(var i = 0; i < arr.length; i++){
+    for(var i = 0; i < arr.length; i++){
         if(arr[i] < currentValue){
-        currentValue = arr[i];
-        }}
+            currentValue = arr[i];
+        }
     }
-minValue();
-console.log('min:' + ' ' + currentValue)
-
-//Сумма
-
-function sum(){
-    var summ = 0;
-      for (var i = 0; i<arr.length; i++){
-        summ +=arr[i];
-      }
-    console.log('Сумма:' + ' ' + summ)
+    return currentValue;
 }
-sum()
+
+console.log('min:' + ' ' + minValue(arr))
+
+//Sum
+
+
+function sum(arr){
+    var sumResult = 0;
+        for (var i = 0; i<arr.length; i++){
+            sumResult +=arr[i];
+        }
+    return sumResult
+}
+console.log('Sum:' + ' ' + sum(arr))
