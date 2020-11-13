bfb60d748ef8a4805d61ce6519b8b7885910c879
Silencia17 <60505498+Silencia17@users.noreply.github.com>
2020-11-13 10:02:04 +0200

    Update task 2.js

--- homework 1/task 2.js
+++ homework 1/task 2.js
@@ -1,24 +1,28 @@
 var height = [2,1,5,0,3,4,7,2,3,1,0];
 
-	var leftMax = 0;
-	var rightMax = 0;
-	var left = 0;
-	var right = height.length - 1;
-	var volume = 0;
-		
-		while(left < right) {
-			if(height[left] > leftMax) {
-				leftMax = height[left];
-			}
-			if(height[right] > rightMax) {
-				rightMax = height[right];
-			}
-			if(leftMax >= rightMax) {
-				volume += rightMax - height[right];
-				right--;
-			} else {
-				volume += leftMax - height[left];
-				left++;
-			}
-		}
-    console.log(volume)
+var leftMax = 0;
+var rightMax = 0;
+var left = 0;
+var right = height.length - 1;
+var volume = 0;
+
+function waterLevel(){
+while(left < right) {
+	if(height[left] > leftMax) {
+	   leftMax = height[left];
+	}
+	if(height[right] > rightMax) {
+	   rightMax = height[right];
+	}
+	if(leftMax >= rightMax) {
+	   volume += rightMax - height[right];
+	   right--;
+	} else {
+	   volume += leftMax - height[left];
+	   left++;
+	}
+ }
+  return volume;
+}
+
+console.log(waterLevel())
