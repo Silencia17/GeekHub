873eb4e6cc279eb9d3b9a7fde41acc9fa1fceb7e
Silencia17 <60505498+Silencia17@users.noreply.github.com>
2020-11-24 13:47:07 +0200

    Update script.js

--- homework 2/tamagotchi/script.js
+++ homework 2/tamagotchi/script.js
@@ -1,14 +1,16 @@
-function Tamagotchi(hungry,health,happiness,toilet,sleep, purity) {
-    this.hungry = hungry;
-    this.health = health;
-    this.happiness = happiness;
-    this.toilet = toilet;
-    this.sleep = sleep;
-    this.purity = purity;
-    this.name = name;
+function Tamagotchi(hungry,health,happiness,toilet,sleep, purity,name) {
+
+    this.hungry = 100;
+    this.health = 100;
+    this.happiness = 100;
+    this.toilet = 100;
+    this.sleep = 100;
+    this.purity = 100;
+    this.name = 'Alice';
 
     this.voice = function () {
         console.log("Привет,я твой новый друг,меня зовут " + this.name)
+        console.log(this)
     }
 
     this.walk = function () {
@@ -19,8 +21,10 @@ function Tamagotchi(hungry,health,happiness,toilet,sleep, purity) {
             this.sleep -= Math.floor(Math.random() * 5);
             this.purity -= Math.floor(Math.random() * 3);
             this.hungry -= Math.floor(Math.random() * 5);
+            console.log(this)
         } else {
             console.log('Я итак счастлив!)');
+            console.log(this)
         }
     }
 
@@ -30,19 +34,24 @@ function Tamagotchi(hungry,health,happiness,toilet,sleep, purity) {
             this.happiness -= Math.floor(Math.random() * 7);
             this.sleep -= Math.floor(Math.random() * 5);
             this.health += Math.floor(Math.random() * 6);
+            console.log(this)
         } else {
             console.log('Я итак здоров!)')
+            console.log(this)
         }
     }
     this.goToilet = function (){
         if(this.toilet <= 92){
-          this.toilet += Math.floor(Math.random() * 8);
-          this.happiness += Math.floor(Math.random() * 2);
-          this.hungry -= Math.floor(Math.random() * 2);
-          this.purity -= Math.floor(Math.random() * 5);
+            this.toilet += Math.floor(Math.random() * 8);
+            this.happiness += Math.floor(Math.random() * 2);
+            this.hungry -= Math.floor(Math.random() * 2);
+            this.purity -= Math.floor(Math.random() * 5);
+            console.log(this)
         }
         else{
-         console.log('Я не хочу в туалет!');     
+            console.log('Я не хочу в туалет!');
+            console.log(this)
+        }
     }
     this.sleeping = function (){
         if(this.sleep <= 92){
@@ -50,17 +59,21 @@ function Tamagotchi(hungry,health,happiness,toilet,sleep, purity) {
             this.happiness += Math.floor(Math.random() * 2);
             this.hungry -= Math.floor(Math.random() * 6);
             this.purity -= Math.floor(Math.random() * 5);
+            console.log(this)
         }
         else{
             console.log('я не хочу спать!)')
+            console.log(this)
         }
     }
     this.eating = function (){
         if(this.hungry <= 95){
             this.hungry += Math.floor(Math.random() * 6);
+            console.log(this)
         }
         else {
             console.log('Я не хочу есть!)')
+            console.log(this)
         }
     }
     this.getShower = function (){
@@ -68,9 +81,11 @@ function Tamagotchi(hungry,health,happiness,toilet,sleep, purity) {
             this.purity += Math.floor(Math.random() * 5);
             this.hungry -= Math.floor(Math.random() * 4);
             this.happiness += Math.floor(Math.random() * 2);
+            console.log(this)
         }
         else{
             console.log('Давай немного позже')
+            console.log(this)
         }
     }
     this.run = function (){
@@ -81,29 +96,37 @@ function Tamagotchi(hungry,health,happiness,toilet,sleep, purity) {
         this.toilet = 0;
         this.sleep = 0;
         this.purity = 0;
+        console.log(this)
     }
-    this.end = function (){
-        if(this.hungry === 0 || this.health === 0 || this.happiness === 0 || this.toilet === 0 || this.sleep === 0 || this.purity === 0 ){
-            console.log('Я умер............')
-             window.location.reload();
-        }
-        else {
-            console.log('Dead')
-        }
-    }
+     // this.end = function (){
+
+    //     else {
+    //         console.log('Dead')
+    //     }
+  //  }
 }
+    var pet = new Tamagotchi();
 
-
-    var pet = new Tamagotchi(100, 100, 100, 100, 100, 100, 'Alice');
-
-        setInterval(function propInt(Time = 2) {
-        pet.hungry = pet.hungry - parseInt(Time);
-        pet.health = pet.health - parseInt(Time);
-        pet.happiness = pet.happiness - parseInt(Time);
-        pet.toilet = pet.toilet - parseInt(Time);
-        pet.sleep = pet.sleep - parseInt(Time);
-        pet.purity = pet.purity - parseInt(Time);
+        setInterval(function propInt(time = 2) {
+        pet.hungry = pet.hungry - parseInt(time);
+        pet.health = pet.health - parseInt(time);
+        pet.happiness = pet.happiness - parseInt(time);
+        pet.toilet = pet.toilet - parseInt(time);
+        pet.sleep = pet.sleep - parseInt(time);
+        pet.purity = pet.purity - parseInt(time);
     }, 5000)
+
+     // if(pet.hungry === 0 || pet.health === 0 || pet.happiness === 0 || pet.toilet === 0 || pet.sleep === 0 || pet.purity === 0 ){
+     //     alert('я умер')
+     // }
+     // else {
+     //     console.log('Dead')
+     // }
+//
+// if (this.hungry === 0 || this.health === 0 || this.happiness === 0 || this.toilet === 0 || this.sleep === 0 || this.purity === 0 ){
+//            alert('edqwdqwd');
+//        }
+
 
     pet.voice();
     // pet.walk();
